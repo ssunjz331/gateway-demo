d2d28aa03a4651cc7c44b058af7a193bb7bc3eab
Sunjung Jang <ssunjz331@gmail.com>
2020-11-18 15:26:24 +0900

    updated temp run

--- config-server (1)/config-server/src/main/java/com/direa/configserver/ConfigServerApplication.java
+++ config-server (1)/config-server/src/main/java/com/direa/configserver/ConfigServerApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 //@EnableAutoConfiguration
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+//		System.setProperty("spring.profiles.default", "local");
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
