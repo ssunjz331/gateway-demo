fce8d684e70670f65f77a77ff292b5bc7a14cb9e
Sunjung Jang <ssunjz331@gmail.com>
2020-11-20 13:33:43 +0900

    fallback

--- zuul (1)/zuul/src/main/java/com/direa/zuul/fallback/GatewayServiceFallback.java
+++ zuul (1)/zuul/src/main/java/com/direa/zuul/fallback/GatewayServiceFallback.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 class GatewayServiceFallback implements FallbackProvider {
 
-    private static final String DEFAULT_MESSAGE = "Service not available."; //라우팅 실패 에러
+    private static final String DEFAULT_MESSAGE = "Service not available."; //Fallback for Routing Error
 
     @Override
     public String getRoute() {
